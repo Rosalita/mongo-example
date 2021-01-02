b77c0d097db9eda7c24315d54ae139ffd89d0895
Rosie Hamilton <enchantrix@gmail.com>
2021-01-02 13:58:32 +0000

    working example of a mongo aggregation pipeline

--- aggregation.go
+++ aggregation.go
@@ -5,10 +5,9 @@ import (
 	"fmt"
 	"log"
 
-//	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-//	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // Podcast represents the schema for the "Podcasts" collection
@@ -34,29 +33,73 @@ func aggregate(c *mongo.Client) {
 	col := database.Collection("episodes")
 
 	// seed some data into the database
+	//seedData(col)
 
-	id1, err := primitive.ObjectIDFromHex("5e3b37e51c9d4400004117e6")
-	id2, err := primitive.ObjectIDFromHex("5e3b37e51c9d4400004117e6")
+	podcastID, _ := primitive.ObjectIDFromHex("5e3b37e51c9d4400004117e6")
+
+	// Matching stage that matches all documents that have a specific podcastID
+	matchStage := bson.D{{
+		Key: "$match",
+		Value: bson.D{{
+			Key:   "podcast",
+			Value: podcastID,
+		}},
+	}}
+
+	// Grouping stage groups the data by podcastID, which means there's only one result in the group.
+	// Then it sums all the episode durations for each item in the group.
+	groupStage := bson.D{{
+		Key: "$group",
+		Value: bson.D{
+			{
+				Key:   "_id",
+				Value: "$podcast",
+			},
+			{
+				Key: "total",
+				Value: bson.D{
+					{
+						Key:   "$sum",
+						Value: "$duration",
+					},
+				},
+			},
+		},
+	}}
+
+	showInfoCursor, err := col.Aggregate(context.TODO(), mongo.Pipeline{matchStage, groupStage})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var showsWithInfo []bson.M
+	if err = showInfoCursor.All(context.TODO(), &showsWithInfo); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(showsWithInfo)
+}
+
+func seedData(c *mongo.Collection) {
+	podcastID, _ := primitive.ObjectIDFromHex("5e3b37e51c9d4400004117e6")
 
 	episodes := []interface{}{
 		Episode{
-			Podcast:     id1,
+			Podcast:     podcastID,
 			Title:       "Episode #1",
 			Description: "The first episode",
 			Duration:    25,
 		},
 		Episode{
-			Podcast:     id2,
+			Podcast:     podcastID,
 			Title:       "Episode #2",
 			Description: "The second episode",
 			Duration:    30,
 		},
 	}
 
-	insertManyResult, err := col.InsertMany(context.TODO(), episodes)
+	insertManyResult, err := c.InsertMany(context.TODO(), episodes)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Inserted many documents: ", insertManyResult.InsertedIDs)
-
 }
